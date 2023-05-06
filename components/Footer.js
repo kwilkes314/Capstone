3dbc8986b5c7e04973beccc30930a7e184c7f9e4
kwilkes314 <kendallwilkes.kw@gmail.com>
2023-05-06 11:01:05 -0500

    convert too spa

---
+++ components/Footer.js
@@ -0,0 +1,7 @@
+import html from "html-literal";
+
+export default () => html`
+
+<footer>
+  &copy; 2023 <a href="./index.html">Above Average Code</a>
+</footer>`;
---
+++ components/views/Home.js
@@ -0,0 +1,11 @@
+import html from "html-literal";
+
+export default (state) => html`
+
+<section id="jumbotron">
+    <h2>Above Average Code</h2>
+    <a href="index.html">Login</a>
+  </section>
+    <h3>
+    The weather in ${state.weather.city} is ${state.weather.description}. Temperature is ${state.weather.temp}F, and it feels like ${state.weather.feelsLike}F.
+  </h3>`;
